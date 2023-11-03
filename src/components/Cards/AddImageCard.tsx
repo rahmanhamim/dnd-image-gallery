32b167f73dd6535e74c509c3355327bbf6304a08
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-11-03 15:08:46 +0600

    add image card updated

--- src/components/Cards/AddImageCard.tsx
+++ src/components/Cards/AddImageCard.tsx
@@ -5,11 +5,13 @@ const AddImageCard = () => {
   return (
     <div
       className={twMerge(
-        "rounded-lg border border-dashed border-gray-300 flex flex-col items-center justify-center cursor-pointer hover:bg-gray-100 transition-colors duration-500 aspect-square"
+        "rounded-lg border border-dashed border-gray-300 flex flex-col items-center justify-center cursor-pointer hover:bg-gray-100 transition-colors duration-500 aspect-square p-8"
       )}
     >
       <ImageIcon />
-      <p className="font-semibold text-xs md:text-base">Add Images</p>
+      <p className="font-semibold text-xs md:text-base whitespace-nowrap">
+        Add Images
+      </p>
     </div>
   );
 };
